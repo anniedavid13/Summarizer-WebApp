1a7d39013d0e90326cfe6fadebfc761260157135
Annie David <E20CSE068@bennett.edu.in>
2023-04-22 13:45:13 +0530

    edited the demo section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import logo2 from '../assets/logo2.png'
+import logo from '../assets/logo.svg'
 const Hero = () => {
   return (
    <header className='w-full flex justify-center items-center flex-col'>
     <nav className='flex justify-between items-center w-full mb-10 pt-3'>
-      <img src = {logo2} alt = 'logo' className = 'p-6 w-23 object-contain pt-8' />
+      <img src = {logo} alt = 'logo' className = 'p-6 w-23 object-contain pt-8' />
       <button type = "button" className='black_btn' onClick={() => window.open('https://github.com/anniedavid13/Summarizer-WebApp')} > Github  </button>
     </nav>
     <h1 className='head_text items-center'> 
