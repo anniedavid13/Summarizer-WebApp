3f8c623c659ed0b94602db0fab799d25b75bc793
Annie David <E20CSE068@bennett.edu.in>
2023-04-22 16:34:03 +0530

    integrated api and set up demo

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,16 +1,49 @@
 import React from 'react'
 import { useState, useEffect} from 'react'
 import { copy, linkIcon, loader, tick } from '../assets'
-
+import { useLazyGetSummaryQuery } from '../services/article'
 const Demo = () => {
   const [article, setArticle] = useState({
     url:'',
     summary: '',
   });
 
+  const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
+  const [allArticles, setArticles] = useState([]);
+
+  // Load data from localStorage on mount
+  useEffect(() => {
+    const articlesFromLocalStorage = JSON.parse(
+      localStorage.getItem("articles")
+    );
+
+    if (articlesFromLocalStorage) {
+      setArticles(articlesFromLocalStorage);
+    }
+  }, []);
+
   const handleSubmit = async (e) => {
-    
-  }
+    e.preventDefault();
+
+    // const existingArticle = allArticles.find(
+    //   (item) => item.url === article.url
+    // );
+
+    // if (existingArticle) return setArticle(existingArticle);
+
+    const { data } = await getSummary({ articleUrl: article.url });
+    if (data?.summary) {
+      const newArticle = { ...article, summary: data.summary };
+      const updatedAllArticles = [newArticle, ...allArticles];
+
+      // update state and local storage
+      setArticle(newArticle);
+      setArticles(updatedAllArticles);
+      localStorage.setItem("articles", JSON.stringify(updatedAllArticles));
+      
+    }
+
+  };
   return (
     
     <section className='mt-16 w-full max-w-xl'>
@@ -33,6 +66,20 @@ const Demo = () => {
           
            </form>
            {/* Browse URL history */}
+           <div className='flex flex-col gap-1 max-h-60 overflow-y-auto'>
+            {allArticles.map((item,index) => (
+              <div key={`link-${index}`} 
+              onClick ={() => setArticle(item)}
+              className='link_card'
+              >
+                <img src={copy} alt='copy' className='w-[40] h-[40] object-contain' />
+                <p className='flex-1 font-satoshi text-blue-700
+                font-medium text-sm truncate'>
+                  {item.url}
+                  </p>
+                </div>
+            ))}
+           </div>
 
       </div>
 
